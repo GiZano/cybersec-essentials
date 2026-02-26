541e00287eb0ac5b7a738a1e5af46447c8dd6e97
GiZano <giozanotti07@gmail.com>
2026-02-26 12:50:30 +0100

    Add: solution for problem Web 05

---
+++ web-security/01-introduction/05-get-with-manual-cookies.py
@@ -0,0 +1,21 @@
+"""
+Problem: Web 05 - HTTP: Manual cookies
+
+Category: Web Security
+
+Description: Get flag by sending the cookie 'password' with value 'admin'
+
+Algorith:
+- Create dict with cookies (password = 'admin')
+- Receive GET data
+- Show data
+
+"""
+
+import requests
+
+cookies = dict(password='admin')
+
+response = requests.get("http://web-05.challs.olicyber.it/flag", cookies=cookies)
+
+print(response.text)
